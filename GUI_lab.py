0ead1d45ced7ca3118d8f2c2247899363146787b
LuisMaEs <luismamanezginer@gmail.com>
2021-11-17 17:45:55 +0100

    Muhchísmos cambios, se ha implementado la función de barrido contínuo

--- GUI_lab.py
+++ GUI_lab.py
@@ -8,71 +8,154 @@ import time # Necesario para usar delays.
 rm = pyvisa.ResourceManager() # Creamos el objeto Resource Manager
 print(rm.list_resources()) # Imprimimos la lista de recursos: se entiende recursos por instrumentos conectados al pc.
 
-
-Power=0
-def SwitchOnOff():
-    global Power
-    inst = rm.open_resource('GPIB0::20::INSTR')
-    if Power==0:
-        
-        mensaje='SOURCE1:CHAN1:POW:STATE 1'
-        inst.write(mensaje)
-        btn_on_off.config(bg='green')
-        btn_on_off.config(text='State : On')
-        Power=1
-        
-    else:
-        
-        mensaje='SOURCE1:CHAN1:POW:STATE 0'
-        inst.write(mensaje)
-        btn_on_off.config(bg='red')
-        btn_on_off.config(text='State : Off')
-        Power=0
-     
-    time.sleep(2)
-    print(inst.query('SYST:ERR?'))
-    inst.close
-
-
-
       
+class Page(tk.Frame):
+    def __init__(self, *args, **kwargs):
+        tk.Frame.__init__(self, *args, **kwargs)
+    def show(self):
+        self.lift()
 
-#btn_on_off['state'] = NORMAL
-    
 
-def GoToLambda():
-    
-    if bool(entry_var.get())==1:
-        if  1520<float(entry_var.get())<1630:
-            inst = rm.open_resource('GPIB0::20::INSTR')
-            mensaje='SOURCE1:CHAN1:WAVE ' + entry_var.get() + 'NM'
+class Page1(Page):
+    def __init__(self, *args, **kwargs):
+        Page.__init__(self, *args, **kwargs)
+        
+        btn_on_off= tk.Button(self, text='State : Off',command=lambda widget="btn_on_off" : SwitchOnOff(btn_on_off) ,bg='red') 
+        btn_on_off.pack(side="top", fill="both", expand=True)
+
+        ELambda=tk.Entry(self,textvariable=entry_var)
+        ELambda.pack(side="top", fill="both", expand=True)
+
+        btn_set_lambda=tk.Button(self, text='Go to Lambda', command=GoToLambda )
+        btn_set_lambda.pack(side="top", fill="both", expand=True)
+ 
+
+class Page2(Page):
+   def __init__(self, *args, **kwargs):
+       Page.__init__(self, *args, **kwargs)
+       #label = tk.Label(self, text="This is page 2")
+       #label.pack(side="top", fill="both", expand=True)
+
+       label_lambda_min=tk.Label(self,text='Longitud de onda inicial')
+       label_lambda_min.grid(row=0,column=0,ipadx=5,ipady=15)
+       entry_lambda_min=tk.Entry(self,textvariable=entry_var1)
+       entry_lambda_min.grid(row=0,column=1,ipadx=5,ipady=15)
+       
+       label_lambda_max=tk.Label(self,text='Longitud de onda final')
+       label_lambda_max.grid(row=1,column=0,ipadx=5,ipady=15)
+       entry_lambda_max=tk.Entry(self,textvariable=entry_var2,)
+       entry_lambda_max.grid(row=1,column=1,ipadx=5,ipady=15)
+       
+       label_lambda_speed=tk.Label(self,text='Velocidad del barrido')
+       label_lambda_speed.grid(row=2,column=0,ipadx=5,ipady=15)
+       entry_lambda_speed=tk.Entry(self,textvariable=entry_var3)
+       entry_lambda_speed.grid(row=2,column=1,ipadx=5,ipady=15)
+       
+
+       btn_barrido_cont=tk.Button(self, text='Hacer barrido', command=BarridoContinuo)
+       btn_barrido_cont.grid(row=3,column=0,columnspan=3,ipadx=5,ipady=15)
+
+
+
+
+
+class Page3(Page):
+   def __init__(self, *args, **kwargs):
+       Page.__init__(self, *args, **kwargs)
+       label = tk.Label(self, text="This is page 3")
+       label.pack(side="top", fill="both", expand=True)
+
+class MainView(tk.Frame):
+    def __init__(self, *args, **kwargs):
+        tk.Frame.__init__(self, *args, **kwargs)
+        p1 = Page1(self)
+        p2 = Page2(self)
+        p3 = Page3(self)
+        
+
+        buttonframe = tk.Frame(self)
+        container = tk.Frame(self)
+        buttonframe.pack(side="top", fill="x", expand=False)
+        container.pack(side="top", fill="both", expand=True)
+
+        p1.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
+        p2.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
+        p3.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
+
+        b1 = tk.Button(buttonframe, text="Page 1", command=p1.show)
+        b2 = tk.Button(buttonframe, text="Page 2", command=p2.show)
+        b3 = tk.Button(buttonframe, text="Page 3", command=p3.show)
+
+        b1.pack(side="left")
+        b2.pack(side="left")
+        b3.pack(side="left")
+        p1.show()    
+
+if __name__ == "__main__":
+
+
+    def GoToLambda():
+        if bool(entry_var.get())==1:
+            if  1520<float(entry_var.get())<1630:
+                inst = rm.open_resource('GPIB0::20::INSTR') 
+                inst.read_termination = '\n'
+                inst.write_termination = '\n'
+                mensaje='SOURCE1:CHAN1:WAVE ' +entry_var.get()+ 'NM'
+                inst.write(mensaje)
+            else:
+                print('Longitud de onda fuera de rango. Rango válido de 1521 nm a 1629 nm. \n')
+        inst.close()
+    Power=0
+
+
+    def SwitchOnOff(button):
+        global Power
+        inst = rm.open_resource('GPIB0::20::INSTR') 
+        inst.read_termination = '\n'
+        inst.write_termination = '\n'
+        if Power==0:
+        
+            mensaje='SOURCE1:CHAN1:POW:STATE 1'
             inst.write(mensaje)
-            print(inst.query('SYST:ERR?'))
+            button.config(bg='green')
+            button.config(text='State : On')
+            Power=1
+        
+        else:
+        
+            mensaje='SOURCE1:CHAN1:POW:STATE 0'
+            inst.write(mensaje)
+            button.config(bg='red')
+            button.config(text='State : Off')
+            Power=0
+        inst.close()
+        time.sleep(1)  
+    
+    def BarridoContinuo():
+        orden=[]
+        if  1520<float(entry_var1.get())<1630 and 1520<float(entry_var2.get())<1630 and float(entry_var1.get())<float(entry_var2.get()):
+            inst = rm.open_resource('GPIB0::20::INSTR') 
+            inst.read_termination = '\n'
+            inst.write_termination = '\n'
+            orden.append('SOURCE1:CHAN1:WAV:SWE:MODE CON')
+            orden.append('SOURCE1:CHAN1:WAV:SWE:STAR ' + entry_var1.get() + 'NM')
+            orden.append('SOURCE1:CHAN1:WAV:SWE:STOP ' + entry_var2.get() + 'NM')
+            orden.append('SOURCE1:CHAN1:WAV:SWE:SPE ' + entry_var3.get() + 'nm/s')
+            for i in orden: 
+                inst.write(i)
+                print(inst.query('SYST:ERR?'))
+            inst.query('SOURCE1:CHAN1:WAV:SWE STAR')
             inst.close()
 
-        else:
-            print('Longitud de onda fuera de rango. Rango válido de 1521 nm a 1629 nm. \n')
+
+    root = tk.Tk()
+    entry_var = tk.StringVar()
+    entry_var1 = tk.StringVar()
+    entry_var2 = tk.StringVar()
+    entry_var3 = tk.StringVar()
+    main = MainView(root)
+    main.pack(side="top", fill="both", expand=True)
+    root.wm_geometry("400x400")
+    root.mainloop()
+
     
-
-
-
-window= tk.Tk()
-window.title('Laboratorio Biosensado')
-window.geometry('600x400')
-entry_var = tk.StringVar()
-ELambda=tk.Entry(window,textvariable=entry_var)
-ELambda.grid(column=0,row=1)
-#Algunas variables utilizadas
-
-
-
-btn_on_off= tk.Button(window, text='State : Off',command=SwitchOnOff,bg='red') 
-
-btn_on_off.grid(column=2, row=2)
-btn_set_lambda=tk.Button(window, text='Go to Lambda', command=GoToLambda)
-btn_set_lambda.grid(column=1, row=1)
-
-
-
-
-window.mainloop()
--- GUI_multipagina.py
+++ GUI_multipagina.py
@@ -1,6 +1,12 @@
 import tkinter as tk
-
+import time
 # Ejemplo con páginas 
+
+
+
+
+
+      
 
 class Page(tk.Frame):
     def __init__(self, *args, **kwargs):
@@ -8,25 +14,55 @@ class Page(tk.Frame):
     def show(self):
         self.lift()
 
+
 class Page1(Page):
-   def __init__(self, *args, **kwargs):
-       Page.__init__(self, *args, **kwargs)
-       label = tk.Label(self, text="This is page 1")
-       label.pack(side="top", fill="both", expand=True)
+    def __init__(self, *args, **kwargs):
+        Page.__init__(self, *args, **kwargs)
+        
+        btn_on_off= tk.Button(self, text='State : Off',command=lambda widget="btn_on_off" : SwitchOnOff(btn_on_off) ,bg='red') 
+        btn_on_off.pack(side="top", fill="both", expand=True)
+
+        ELambda=tk.Entry(self,textvariable=entry_var)
+        ELambda.pack(side="top", fill="both", expand=True)
+
+        btn_set_lambda=tk.Button(self, text='Go to Lambda', command=GoToLambda )
+        btn_set_lambda.pack(side="top", fill="both", expand=True)
+ 
 
 class Page2(Page):
    def __init__(self, *args, **kwargs):
        Page.__init__(self, *args, **kwargs)
-       label = tk.Label(self, text="This is page 2")
-       label.pack(side="top", fill="both", expand=True)
+       #label = tk.Label(self, text="This is page 2")
+       #label.pack(side="top", fill="both", expand=True)
+
+       label_lambda_min=tk.Label(self,text='Longitud de onda inicial')
+       label_lambda_min.grid(row=0,column=0,ipadx=5,ipady=15)
+       entry_lambda_min=tk.Entry(self,textvariable=entry_var1)
+       entry_lambda_min.grid(row=0,column=1,ipadx=5,ipady=15)
+       
+       label_lambda_max=tk.Label(self,text='Longitud de onda final')
+       label_lambda_max.grid(row=1,column=0,ipadx=5,ipady=15)
+       entry_lambda_max=tk.Entry(self,textvariable=entry_var2,)
+       entry_lambda_max.grid(row=1,column=1,ipadx=5,ipady=15)
+       
+       label_lambda_speed=tk.Label(self,text='Velocidad del barrido')
+       label_lambda_speed.grid(row=2,column=0,ipadx=5,ipady=15)
+       entry_lambda_speed=tk.Entry(self,textvariable=entry_var3)
+       entry_lambda_speed.grid(row=2,column=1,ipadx=5,ipady=15)
+       
+
+       btn_barrido_cont=tk.Button(self, text='Hacer barrido', command=BarridoContinuo)
+       btn_barrido_cont.grid(row=3,column=0,columnspan=3,ipadx=5,ipady=15)
+
+
+
+
 
 class Page3(Page):
    def __init__(self, *args, **kwargs):
        Page.__init__(self, *args, **kwargs)
        label = tk.Label(self, text="This is page 3")
        label.pack(side="top", fill="both", expand=True)
-       label2 = tk.Label(self, text="Página 3")
-       label2.pack(side="top", fill="both", expand=True)
 
 class MainView(tk.Frame):
     def __init__(self, *args, **kwargs):
@@ -34,6 +70,7 @@ class MainView(tk.Frame):
         p1 = Page1(self)
         p2 = Page2(self)
         p3 = Page3(self)
+        
 
         buttonframe = tk.Frame(self)
         container = tk.Frame(self)
@@ -51,14 +88,62 @@ class MainView(tk.Frame):
         b1.pack(side="left")
         b2.pack(side="left")
         b3.pack(side="left")
-
-        p1.show()
-
-
+        p1.show()    
 
 if __name__ == "__main__":
+
+
+    def GoToLambda():
+        if bool(entry_var.get())==1:
+            if  1520<float(entry_var.get())<1630:
+                mensaje='SOURCE1:CHAN1:WAVE ' + entry_var.get() + 'NM'
+                print(mensaje)
+
+            else:
+                print('Longitud de onda fuera de rango. Rango válido de 1521 nm a 1629 nm. \n')
+
+    Power=0
+    def SwitchOnOff(button):
+        global Power
+
+        if Power==0:
+        
+            mensaje='SOURCE1:CHAN1:POW:STATE 1'
+            print(mensaje)
+            button.config(bg='green')
+            button.config(text='State : On')
+            Power=1
+        
+        else:
+        
+            mensaje='SOURCE1:CHAN1:POW:STATE 0'
+            print(mensaje)
+            button.config(bg='red')
+            button.config(text='State : Off')
+            Power=0
+     
+        time.sleep(1)  
+    
+    def BarridoContinuo():
+        orden=[]
+        if  1520<float(entry_var1.get())<1630 and 1520<float(entry_var2.get())<1630 and float(entry_var1.get())<float(entry_var2.get()):
+            orden.append('SOURCE1:CHAN1:WAV:SWE:MODE CON')
+            orden.append('SOURCE1:CHAN1:WAV:SWE:STAR ' + entry_var1.get() + 'NM')
+            orden.append('SOURCE1:CHAN1:WAV:SWE:STOP ' + entry_var2.get() + 'NM')
+            orden.append('SOURCE1:CHAN1:WAV:SWE:SPE ' + entry_var3.get() + 'nm/s')
+            for i in orden: 
+                print(i)
+            print('SOURCE1:CHAN1:WAV:SWE STAR')
+
+
     root = tk.Tk()
+    entry_var = tk.StringVar()
+    entry_var1 = tk.StringVar()
+    entry_var2 = tk.StringVar()
+    entry_var3 = tk.StringVar()
     main = MainView(root)
     main.pack(side="top", fill="both", expand=True)
     root.wm_geometry("400x400")
     root.mainloop()
+
+    
